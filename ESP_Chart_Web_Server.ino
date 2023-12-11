69cd50cd3f207a217a2f5bd2936c31a6439da5cc
claudiafodor <126708401+claudiafodor@users.noreply.github.com>
2023-12-11 09:20:50 -0500

    rftgyhujikolp;

--- ESP_Chart_Web_Server.ino
+++ ESP_Chart_Web_Server.ino
@@ -1,16 +1,7 @@
 // Import required libraries
-#ifdef ESP32
-  #include <WiFi.h>
-  #include <ESPAsyncWebServer.h>
-  #include <SPIFFS.h>
-#else
-  #include <Arduino.h>
-  #include <ESP8266WiFi.h>
-  #include <Hash.h>
-  #include <ESPAsyncTCP.h>
-  #include <ESPAsyncWebServer.h>
-  #include <FS.h>
-#endif
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
